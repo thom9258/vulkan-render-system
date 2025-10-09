cef2a60b44dd71f38eabf1a4a1c2092f7c7ea74f
Thomas Alexgaard <thomasjensen9258@gmail.com>
2025-10-09 23:15:56 +0200

    updated version

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 
 class VulkanRendererRecipe(ConanFile):
     name = "vulkan-renderer"
-    version = "1.3"
+    version = "1.4"
 
     # Optional metadata
     license = "<Put the package license here>"
